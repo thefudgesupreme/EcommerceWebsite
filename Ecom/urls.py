b8708a98301411b3d1c9678860d44fbb2228c188
thefudgesupreme <Vipul.rt99@gmail.com>
2020-07-01 19:33:25 +0530

    Class-Based View Added

--- Ecom/urls.py
+++ Ecom/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import itemList
+from .views import CheckoutView, ProductView, HomeView,home,checkout
 
 app_name = 'Ecom'
 
 urlpatterns = [
-    path('', itemList, name='itemList')
+    path('', HomeView.as_view(), name='home'),
+    path('checkout/', checkout, name='Checkout'),
+    path('product/<slug>', ProductView.as_view(), name='Product Details')
 ]
--- Ecom/views.py
+++ Ecom/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from .models import Item
+from django.views.generic import ListView,DetailView,TemplateView
 
-def itemList(request):
+
+def home(request):
     context={
-        'item': Item.objects.all()
+        'items': Item.objects.all()
     }
     return render(request,"homePage.html",context)
+
+def checkout(request):
+    return render(request,'checkoutPage.html')
+
+def products(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request,'productPage.html',context)
+
+class HomeView(ListView):
+    model = Item
+    template_name = "homePage.html"
+
+class CheckoutView(TemplateView):
+    model = Item
+    template_name = "checkoutPage"
+
+class ProductView(DetailView):
+    model = Item
+    template_name = "productPage.html"
